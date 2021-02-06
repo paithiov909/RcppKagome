08c403e8cba2203d2de731d70e87b61dd342308a
Kato Akiru <am11.44@gmail.com>
2021-02-06 10:24:48 +0900

    documentation

--- src/bridge.cpp
+++ src/bridge.cpp
@@ -10,10 +10,10 @@ using namespace Rcpp;
 //' Trigger kagome tokenizer
 //'
 //' For internal use. The argument should be UTF8 encoded. This function just
-//' returns an UTF8-encoded json as character scalar.
+//' returns an UTF8-encoded json as a character scalar.
 //'
-//' @param text character vector
-//' @return res character scalar (JSON string)
+//' @param text Character vector
+//' @return res Character scalar (JSON string)
 //'
 //' @name tokenize
 //' @keywords internal
