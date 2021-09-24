45ba30fc65e9181fcde555addb71850673ae4278
paithiov909 <am11.44@gmail.com>
2021-09-24 17:12:01 +0900

    fix: change behavior on load

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,17 @@
+#' On load
+#' @noRd
+#' @param libname libname
+#' @param pkgname pkgname
+.onLoad <- function(libname, pkgname) {
+  # if (pkgload::is_dev_package(pkgname)) {}
+  return(invisible(NULL))
+}
+
 #' On unload
 #' @noRd
 #' @param libpath libpath
-#' @keywords internal
 .onUnload <- function(libpath) {
   library.dynam.unload("RcppKagome", libpath)
+  library.dynam.unload("libkagome", libpath, file.ext = ".a")
+
 }
