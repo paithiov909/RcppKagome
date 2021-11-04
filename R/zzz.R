7e0c9813f37f7e020ba608193c89d60452934070
paithiov909 <am11.44@gmail.com>
2021-11-04 23:30:48 +0900

    chore: update Makevars

--- R/zzz.R
+++ R/zzz.R
@@ -2,6 +2,6 @@
 #' @noRd
 #' @param libpath libpath
 .onUnload <- function(libpath) {
-  library.dynam.unload("RcppKagome", libpath)
+  # library.dynam.unload("RcppKagome", libpath)
   # library.dynam.unload("libkagome", libpath, file.ext = ".a")
 }
